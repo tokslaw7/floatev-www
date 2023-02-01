f6c44f8b1a0c2f32afa4caebbcf1b31866672244
tokslaw <tokslaw@chiefsoft.com>
2023-02-01 09:37:01 -0500

    FAQ text

--- src/component/HomeMain/Faq/Main.js
+++ src/component/HomeMain/Faq/Main.js
@@ -25,10 +25,9 @@ const Main = ({gredient}) => {
                             </div>
                             <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
                                 <div className="card-body">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry lorem Ipsum has. been the
-                                    industrys standard dummy text ever since the when an unknown printer took a galley of type and
-                                    scrambled it to make a type specimen book. It has survived not only five cen turies but also the
-                                    leap into electronic typesetting, remaining essentially unchanged.</p>
+                                <p> FloatEV is a fleet product for small and medium size enterprises.
+                                    A concept for a new and flexible mobility experience.
+                                    FloatEV maintains and cares for the fleet </p>
                                 </div>
                             </div>
                         </div>
@@ -41,10 +40,10 @@ const Main = ({gredient}) => {
                             </div>
                             <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
                                 <div className="card-body">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry lorem Ipsum has. been the
-                                    industrys standard dummy text ever since the when an unknown printer took a galley of type and
-                                    scrambled it to make a type specimen book. It has survived not only five cen turies but also the
-                                    leap into electronic typesetting, remaining essentially unchanged.</p>
+                                <p>Create an account and order your subscription online at www.floatev.com/getstarted
+                                    <br />Enter your personal data.<br />
+                                        Book your fleet subscription. <br />
+                                        We will then send you a final booking confirmation in your email, resulting in a binding contract between you and FloatEv</p>
                                 </div>
                             </div>
                         </div>
@@ -52,16 +51,13 @@ const Main = ({gredient}) => {
                             <div className="card-header" id="headingThree">
                                 <h2 className="mb-0">
                                 <button type="button" className={`btn btn-link ${activeFaq.c && "active"}`} onClick= {() => setActiveFaq(activeFaq.c ? {c : false} : {c : true})} data-toggle="collapse"
-                                    data-target="#collapseThree">{activeFaq.c ? <i className="icon_faq icofont-minus"></i>  : <i className="icon_faq icofont-plus"></i>}What is process to get refund
+                                    data-target="#collapseThree">{activeFaq.c ? <i className="icon_faq icofont-minus"></i>  : <i className="icon_faq icofont-plus"></i>}What is process ?
                                     ?</button>
                                 </h2>
                             </div>
                             <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
                                 <div className="card-body">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry lorem Ipsum has. been the
-                                    industrys standard dummy text ever since the when an unknown printer took a galley of type and
-                                    scrambled it to make a type specimen book. It has survived not only five cen turies but also the
-                                    leap into electronic typesetting, remaining essentially unchanged.</p>
+                                <p>Subscribe to the plan preferred and we take care of the entire conversion process and provide you with a personalized metrics and analytics system galley of type and</p>
                                 </div>
                             </div>
                         </div>
@@ -69,16 +65,13 @@ const Main = ({gredient}) => {
                             <div className="card-header" id="headingFour">
                                 <h2 className="mb-0">
                                 <button type="button" className={`btn btn-link ${activeFaq.d && "active"}`} onClick= {() => setActiveFaq(activeFaq.d ? {d : false} : {d : true})} data-toggle="collapse"
-                                    data-target="#collapseFour">{activeFaq.d ? <i className="icon_faq icofont-minus"></i>  : <i className="icon_faq icofont-plus"></i>}What is process to get refund
+                                    data-target="#collapseFour">{activeFaq.d ? <i className="icon_faq icofont-minus"></i>  : <i className="icon_faq icofont-plus"></i>}How does the Subscription works?
                                     ?</button>
                                 </h2>
                             </div>
                             <div id="collapseFour" className="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
                                 <div className="card-body">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry lorem Ipsum has. been the
-                                    industrys standard dummy text ever since the when an unknown printer took a galley of type and
-                                    scrambled it to make a type specimen book. It has survived not only five cen turies but also the
-                                    leap into electronic typesetting, remaining essentially unchanged.</p>
+                                <p>The term of your subscription begins as soon as you receive your booked confirmation.<br/> A personal collection  of your numerous fleet.</p>
                                 </div>
                             </div>
                         </div>
