55fa075e1d99a5ecfd564c9d50ecd76737e1dc26
tokslaw <tokslaw@chiefsoft.com>
2023-02-01 09:52:17 -0500

    FAQ text modified

--- src/component/HomeMain/Faq/Main.js
+++ src/component/HomeMain/Faq/Main.js
@@ -11,8 +11,7 @@ const Main = ({gredient}) => {
             <div className="container">
                 <div className="section_title" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300">
                     <h2><span>FAQ</span> - Frequently Asked Questions</h2>
-                    <p>We guide you step by step to convert to Electronic vehicles.<br/> indus orem Ipsum has beenthe
-                        standard dummy.</p>
+                    <p>We guide you step by step to convert to Electronic vehicles.<br/> </p>
                 </div>
                 <div className="faq_panel">
                     <div className="accordion" id="accordionExample">
@@ -27,7 +26,7 @@ const Main = ({gredient}) => {
                                 <div className="card-body">
                                 <p> FloatEV is a fleet product for small and medium size enterprises.
                                     A concept for a new and flexible mobility experience.
-                                    FloatEV maintains and cares for the fleet </p>
+                                    FloatEV maintains and cares for your fleet </p>
                                 </div>
                             </div>
                         </div>
@@ -35,12 +34,12 @@ const Main = ({gredient}) => {
                             <div className="card-header" id="headingTwo">
                                 <h2 className="mb-0">
                                 <button type="button" className={`btn btn-link ${activeFaq.b && "active"}`} onClick= {() => setActiveFaq(activeFaq.b ? {b : false} : {b : true})} data-toggle="collapse"
-                                    data-target="#collapseTwo">{activeFaq.b ? <i className="icon_faq icofont-minus"></i>  : <i className="icon_faq icofont-plus"></i>} How to setup account ?</button>
+                                    data-target="#collapseTwo">{activeFaq.b ? <i className="icon_faq icofont-minus"></i>  : <i className="icon_faq icofont-plus"></i>} How to setup FloatEV account ?</button>
                                 </h2>
                             </div>
                             <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
                                 <div className="card-body">
-                                <p>Create an account and order your subscription online at www.floatev.com/getstarted
+                                    <p>Create an account and order your subscription online at <a href='www.floatev.com'> <i className="icon_faq"></i> FloatEV.</a>
                                     <br />Enter your personal data.<br />
                                         Book your fleet subscription. <br />
                                         We will then send you a final booking confirmation in your email, resulting in a binding contract between you and FloatEv</p>
@@ -51,7 +50,7 @@ const Main = ({gredient}) => {
                             <div className="card-header" id="headingThree">
                                 <h2 className="mb-0">
                                 <button type="button" className={`btn btn-link ${activeFaq.c && "active"}`} onClick= {() => setActiveFaq(activeFaq.c ? {c : false} : {c : true})} data-toggle="collapse"
-                                    data-target="#collapseThree">{activeFaq.c ? <i className="icon_faq icofont-minus"></i>  : <i className="icon_faq icofont-plus"></i>}What is process ?
+                                    data-target="#collapseThree">{activeFaq.c ? <i className="icon_faq icofont-minus"></i>  : <i className="icon_faq icofont-plus"></i>}What is the process
                                     ?</button>
                                 </h2>
                             </div>
@@ -65,7 +64,7 @@ const Main = ({gredient}) => {
                             <div className="card-header" id="headingFour">
                                 <h2 className="mb-0">
                                 <button type="button" className={`btn btn-link ${activeFaq.d && "active"}`} onClick= {() => setActiveFaq(activeFaq.d ? {d : false} : {d : true})} data-toggle="collapse"
-                                    data-target="#collapseFour">{activeFaq.d ? <i className="icon_faq icofont-minus"></i>  : <i className="icon_faq icofont-plus"></i>}How does the Subscription works?
+                                    data-target="#collapseFour">{activeFaq.d ? <i className="icon_faq icofont-minus"></i>  : <i className="icon_faq icofont-plus"></i>}How does the subscription works
                                     ?</button>
                                 </h2>
                             </div>
